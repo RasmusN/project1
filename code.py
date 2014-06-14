3cdda306433c83897681a4cdb4d16cd41dcc676f
RasmusN <nordstrom.rasmus@gmail.com>
2014-06-14 23:44:40 +0200

    Added division

--- code.py
+++ code.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
-def add(n1, n2)
+def add(n1, n2):
 	return n1+n2
+
+def devide(n1, n2):
+	return n1/n2
 
 print "Let's add some numbers"
 
 print "Two plus four equals: %d " %  add(2, 4)
+print "We can also devide: %.2f"  % devide(2,4)
